a01137f14fe07b6e6b59f69354a51f9d3fe4c42e
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 20:21:06 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,6 +6,10 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    Appointment.new
+    Appointment.new(self, doctor, date)
+  end
+  
+  def doctors
+    Appointment.all.select{|appointment| appointment.patient == self}.map{|item| item.doctor}
   end
 end
