55e7f7b6748f132b951b052a587498cff00f977f
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 20:08:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,8 +7,8 @@ class Artist
     @@all << self
   end
   
-  def new_song(title, genre)
-    Song.new(title,self, genre)
+  def new_song(name, genre)
+    Song.new(name,self, genre)
   end
   
   def self.all 
