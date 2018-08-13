1aeab2df86373ed9a3b64e075aefd1491e8ec823
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 20:11:23 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,14 +1,14 @@
 class Genre 
   attr_accessor :name
-  attr_accessor :songs
-  attr_writer :artists
   
   def initialize(name)
     @name = name 
     @songs = []
     @artists = []
   end
-  
+
+  def songs
+  end
   def artists
     @artists = Song.all.collect{|song| song.genre == self}.map{|song| song.artist}
     return @artists
